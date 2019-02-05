f2447916d6f9bac1ceddb30e83a310171e6113dd
fallensnow4 <fallensnow4@gmail.com>
2019-02-05 09:24:17 -0500

    Update linefollowing.ino

--- linefollowing.ino
+++ linefollowing.ino
@@ -1,6 +1,6 @@
 
 /*NOT DONE DON'T TOUCH YET*/
-
+/*
 // Pin definitions
 #define linesensor 1
 #define MOTORL 3
@@ -17,7 +17,7 @@ void setup() {
 void loop() {
  
 }
-
+*/
 //-------SENSOR READING CODE----------
 void readLFMSensors()
 {
@@ -87,4 +87,53 @@ void testLFMSensors()
      Serial.print (D);
      Serial.print (" PID: ");
      Serial.println (PIDvalue);
+}
+
+// SERVO TESTING
+#include <Servo.h>
+
+Servo servoLeft;          // Define left servo
+Servo servoRight;         // Define right servo
+
+void setup() { 
+  servoLeft.attach(10);  // Set left servo to digital pin 10
+  servoRight.attach(9);  // Set right servo to digital pin 9
+} 
+
+void loop() {            // Loop through motion tests
+  forward();             // Example: move forward
+  delay(2000);           // Wait 2000 milliseconds (2 seconds)
+  reverse();
+  delay(2000);
+  turnRight();
+  delay(2000);
+  turnLeft();
+  delay(2000);
+  stopRobot();
+  delay(2000);
+}
+
+// Motion routines for forward, reverse, turns, and stop
+void forward() {
+  servoLeft.write(0);
+  servoRight.write(180);
+}
+
+void reverse() {
+  servoLeft.write(180);
+  servoRight.write(0);
+}
+
+void turnRight() {
+  servoLeft.write(180);
+  servoRight.write(180);
+}
+void turnLeft() {
+  servoLeft.write(0);
+  servoRight.write(0);
+}
+
+void stopRobot() {
+  servoLeft.write(90);
+  servoRight.write(90);
 }
