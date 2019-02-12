aeffdac667e42711dc06092975225d0602ce5cb5
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2019-02-12 17:46:27 -0500

    running prototype

--- line_follwer_test/line_follwer_test.ino
+++ line_follwer_test/line_follwer_test.ino
@@ -3,6 +3,9 @@
 uchar t;
 //void send_data(short a1,short b1,short c1,short d1,short e1,short f1);
 uchar data[16];
+ int line_value;
+ int line_position;
+  
 void setup()
 {
   Wire.begin();        // join i2c bus (address optional for master)
@@ -10,6 +13,7 @@ void setup()
   t = 0;
 }
 void loop()
+
 {
   Wire.requestFrom(9, 16);    // request 16 bytes from slave device #9
   while (Wire.available())   // slave may send less than requested
@@ -20,6 +24,7 @@ void loop()
     else
       t = 0;
   }
+  /*
   Serial.print("data[0]:");
   Serial.println(data[0]);
   Serial.print("data[2]:");
@@ -37,4 +42,18 @@ void loop()
   Serial.print("data[14]:");
   Serial.println(data[14]);
   delay(500);
+  */
+  line_value = 200;
+  line_position = -1;
+  int i = 0 ;
+  for (i=0; i < 15; i+=2){
+    if (data[i] < line_value){
+      line_position = i ;
+      line_value = data[i];
+    }
+  }
+  Serial.print(line_position);
+  Serial.println();
+  delay(1);
+  
 }
